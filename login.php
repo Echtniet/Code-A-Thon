f5c73eb49cbc9df0539299d5e56298fc11373302
Clinton Davelaar <DavelaarProgramming@gmail.com>
2018-10-20 21:38:37 -0500

    login

--- login.php
+++ login.php
@@ -12,13 +12,25 @@
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	} 
-	$sql = "SELECT COUNT(*) AS found FROM jobseeker WHERE username = ? AND password = ?";
+	$sql = "SELECT COUNT(*) AS found FROM jobseeker WHERE (email =  or username = ?) AND password = ?";
 	$stmt = $conn->prepare($sql);
-	$stmt->bind_param("ss", $info->username, $info->psw);
+	$stmt->bind_param("sss", $info->username, $info->username, $info->psw);
 	$stmt->execute();
 	$stmt->bind_result($found);
 	$stmt->fetch();
-    
+    if ($found == 1) {
+    	//user loging
+    }
+
+    $sql = "SELECT COUNT(*) AS found FROM recruiter WHERE (email = ? or username = ?) AND password = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("sss", $info->username, $info->username, $info->psw);
+	$stmt->execute();
+	$stmt->bind_result($found);
+	$stmt->fetch();
+    if ($found == 1) {
+    	//recuiter loging
+    }
     echo $found;
 	
     $stmt->close();
